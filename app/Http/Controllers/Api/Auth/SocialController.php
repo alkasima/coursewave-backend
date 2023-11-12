11b4fe0b81d7702399cd2e4de638f33768dea235
Kasim Abubakar Jajere <alkasima@gmail.com>
2023-11-12 17:11:01 +0100

    Added google authentication

--- app/Http/Controllers/Api/Auth/SocialController.php
+++ app/Http/Controllers/Api/Auth/SocialController.php
@@ -12,4 +12,38 @@ class SocialController extends Controller
     public function googleDirect() {
         return Socialite::driver('google')->redirect();
     }
+
+    public function callbackGoogle() {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('social_id', $googleUser->getId)->first();
+
+            if(!$user) {
+                $new_user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'social_id' => $googleUser->getId(),
+                ]);
+
+                Auth::login($user);
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User logged in successfully',
+                    'token' => $user->createToken("API TOKEN")->plainTextToken
+                ], 200);
+            }else {
+                
+                Auth::login($user);
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User logged in successfully',
+                    'token' => $user->createToken("API TOKEN")->plainTextToken
+                ], 200);
+            }
+
+        } catch(\Throwable $th) {
+            //Throw $th;
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\Auth\SocialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,9 @@ use App\Http\Controllers\Api\Auth\AuthController;
 
 Route::post('auth/register', [AuthController::class, 'createUser']);
 Route::post('auth/login', [AuthController::class, 'loginUser']);
+Route::get('auth/google', [SocialController::class, 'googleDirect']);
+Route::get('auth/google/call-back', [SocialController::class, 'callbackGoogle']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
